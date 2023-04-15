a45c0af8c37b3c4c28a4c7e3ebb9a27bca937120
Zkirvaldize <giorgi.kirvalidze@georgiatech.eu>
2023-04-15 18:13:48 +0400

    .

--- buffer/src/index.js
+++ buffer/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
+const sharp = require("sharp");
 
 app.get("/buffer-image", function (req, res) {
   // read binary data from a file asynchronously
@@ -19,6 +20,41 @@ app.get("/buffer-image", function (req, res) {
     res.setHeader("Content-Type", "image/png");
     res.setHeader("Content-Length", buffer.length);
     res.end(buffer);
+  });
+});
+
+app.get("/crop-image", (req, res) => {
+  fs.readFile("src/photo.jpeg", (err, buffer) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // Check the size of the input image
+    sharp(buffer).metadata((err, metadata) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      // Set the target size to 400x400 if the input image is larger than that size
+      const targetWidth = metadata.width > 400 ? 400 : metadata.width;
+      const targetHeight = metadata.height > 400 ? 400 : metadata.height;
+
+      // Crop and resize the image using Sharp
+      sharp(buffer)
+        .resize(targetWidth, targetHeight)
+        .toBuffer((err, croppedBuffer) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          // Send the cropped and zoomed-out image data in an HTTP response
+          res.setHeader("Content-Type", "image/jpeg");
+          res.send(croppedBuffer);
+        });
+    });
   });
 });
 
